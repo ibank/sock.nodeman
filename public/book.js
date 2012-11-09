06a0be0145f1e4b7ac4ee94fe6ee6ef5aeb125e6
nanha park <nanhap@gmail.com>
2012-11-09 22:25:57 +0900

    box-shadow

--- public/book.js
+++ public/book.js
@@ -1,6 +1,11 @@
+/**
+ * book mode
+ *
+ * @author nanhapark
+ */
 (function() {
   var s = document.createElement('script');
   s.type = 'text/javascript';
-  s.src = 'http://sock.nodeman.org/book_process.js?' + (+new Date());
+  s.src = 'http://socket.nodeman.org/book_process.js?' + (+new Date());
   document.body.appendChild(s);
 })();
--- public/book_process.js
+++ public/book_process.js
@@ -1,3 +1,8 @@
+/**
+ * book mode
+ *
+ * @author nanhapark
+ */
 var t = document.getElementById('socket.io');
 if (t) document.body.removeChild(t);
 
@@ -10,6 +15,9 @@ s.type = 'text/javascript';
 s.src = WS + '/socket.io/socket.io.js';
 s.onload = function() {
   console.log('io load complete');
+  //
+  // fire keyboard event
+  //
   var fireKey = function(keycode) {
     ["keyup", "keydown"].forEach(function(a) {
       var e = jQuery.Event(a);
@@ -17,9 +25,15 @@ s.onload = function() {
       $(document.body).trigger(e);
     });
   };
-  var h = WS, E = {
-    close:  function() { document.getElementById('qrcode').style.display = 'none'  },
-    left:  function() {
+  
+  //
+  // left, right keyboard action
+  //
+  var E = {
+    close:  function() {
+      document.getElementById('qrcode').style.display = 'none'
+    },
+    left: function() {
       fireKey(37)
     },
     right: function() {
@@ -27,25 +41,10 @@ s.onload = function() {
     }
   };
 
-  var u = WEB + '/control';
-  var s = io.connect(h);
-
-  setTimeout(function() {
-    var o = document.getElementById('qrcode');
-    if (o) document.body.removeChild(o);
-
-    var u2 = u + '/' + s.socket.sessionid;
-    var a = 'http://chart.apis.google.com/chart?cht=qr&chs=500x500&chl=' + u2 + '&chld=H|0';
-    var div = document.createElement('div');
-    div.id = 'qrcode';
-    div.style.position = 'absolute';
-    div.style.top  = '100px';
-    div.style.left = '200px';
-    div.style.zIndex = 100000;
-    div.innerHTML = '<img src="' + a + '">';
-    document.body.appendChild(div);
-  }, 100);
-
+  //
+  // connect websocket
+  //
+  var s = io.connect(WS);
   s.on('return', function(data) {
     if (!E[data]) {
       alert('not found');
@@ -53,5 +52,25 @@ s.onload = function() {
     }
     E[data]()
   });
+  
+  //
+  // show qrcode after received socket sessionid
+  //
+  setTimeout(function() {
+    var o = document.getElementById('qrcode');
+    if (o) document.body.removeChild(o);
+
+    var u2 = WEB + '/control' + s.socket.sessionid;
+    var a = 'http://chart.apis.google.com/chart?cht=qr&chs=500x500&chl=' + u2 + '&chld=H|0';
+    var div = document.createElement('div');
+    div.id = 'qrcode';
+    div.style.position = 'absolute';
+    div.style.top  = '10px';
+    div.style.left = '10px';
+    div.style.boxShadow = '0px 26px 190px rgba(0, 0, 0, 1)';
+    div.style.zIndex = 1000000;
+    div.innerHTML = '<img src="' + a + '" border="0">';
+    document.body.appendChild(div);
+  }, 100);
 };
 document.body.appendChild(s);
